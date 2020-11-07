92a1430efdc252ffa7f2e168054661203bf86b71
ethanzohar <ethanzohar9@gmail.com>
2020-11-07 11:43:17 -0500

    adding express

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const cron = require('node-cron');
+const express = require('express')
+
 const SpotifyHelper = require('./Helpers/SpotifyHelper');
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at port: ${port}`)
+})
 
 cron.schedule('0 6 * * *', () => {
     console.log("Starting job");
