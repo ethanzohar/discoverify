9ec6e7aa9a378561cafbfbe94c709e506244e2c6
ethanzohar <ethanzohar9@gmail.com>
2020-11-28 12:47:32 -0500

    one last fix

--- frontend/src/DiscoverDaily/DiscoverDaily.js
+++ frontend/src/DiscoverDaily/DiscoverDaily.js
@@ -76,7 +76,7 @@ class DiscoverDaily extends Component {
     }
     
     if (code && code !== 'null') {
-      const { access_token, refresh_token } = await SpotifyHelper.getRefreshToken(code, window.location.origin + '/discover-daily/redirect');
+      const { access_token, refresh_token } = await SpotifyHelper.getRefreshToken(code, window.location.origin + '/redirect');
       localStorage.setItem('discoverDaily_refreshToken', refresh_token ? refresh_token : null);
 
       if (!access_token) this.sendToLogin();
