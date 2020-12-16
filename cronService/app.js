ff5250bd8a442c9e397f1fa84455a17ca3c9ed2c
ethanzohar <ethanzohar9@gmail.com>
2020-12-15 21:12:44 -0500

    adding in cron service

---
+++ cronService/app.js
@@ -0,0 +1,34 @@
+const { CronJob } = require('cron');
+const mongoose = require('mongoose');
+
+const UserController = require('../backend/controllers/userController');
+const SpotifyHelper = require('../backend/helpers/spotifyHelper');
+
+mongoose.connect('mongodb://localhost:27017/playlist-generator', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const { connection } = mongoose;
+
+connection.on('connected', () => {
+  console.log('MongoDB database connected');
+});
+
+connection.on('error', () => {
+  console.log('MongoDB Connection Error');
+});
+
+const job = new CronJob(
+  '00 00 00 * * *',
+  async function () {
+    console.log('Starting job');
+    const users = await UserController.getAllUsers();
+    SpotifyHelper.updatePlaylists(users);
+  },
+  null,
+  true,
+  'America/Toronto'
+);
+job.start();
