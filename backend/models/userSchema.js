6e046046b58f2ab422b023d619a0c25b16a9dce3
ethanzohar <ethanzohar9@gmail.com>
2020-12-17 12:09:57 -0500

    trying to fix backend

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -4,8 +4,8 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema({
   userId: { type: String, required: true, unique: true },
-  refreshToken: { type: String, required: true, unique: true },
-  playlistId: { type: String, required: false, unique: false },
+  refreshToken: { type: String, required: true },
+  playlistId: { type: String },
   lastUpdated: { type: Date },
   playlistOptions: {
     seeds: { type: [String], default: ['ST', 'ST', 'MT', 'MT', 'MT'] },
