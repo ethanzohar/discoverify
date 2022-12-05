bd67f51171370f123c5667c1f47260ff698f9fc3
ethanzohar <ethanzohar9@gmail.com>
2022-12-05 16:41:31 +0000

    adding new build

---
+++ frontend/deployedBuildFromLocal/precache-manifest.3325e42b1d0cad7199b46e65e7d0bd96.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b40e2a7438c9c51fe9e9bed1db7ade94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "190a8a9f7655bfa9fe30",
+    "url": "/static/css/main.e316c90a.chunk.css"
+  },
+  {
+    "revision": "0ddcba30e12efeec6153",
+    "url": "/static/js/2.504fc5c2.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/static/js/2.504fc5c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "190a8a9f7655bfa9fe30",
+    "url": "/static/js/main.a316e75e.chunk.js"
+  },
+  {
+    "revision": "adabdd978ef72068cd0b",
+    "url": "/static/js/runtime-main.aa9f6af0.js"
+  },
+  {
+    "revision": "3a47f5f06b5484abfeee9eac90348a9c",
+    "url": "/static/media/Gotham.3a47f5f0.ttf"
+  },
+  {
+    "revision": "5aed1b6624fa3e85b3609e57d91dbc91",
+    "url": "/static/media/Gotham.5aed1b66.woff"
+  },
+  {
+    "revision": "d73e74891f3eea221f54bfa1f2bb23d8",
+    "url": "/static/media/Gotham.d73e7489.woff2"
+  },
+  {
+    "revision": "bafee7d2a1c0abeb62dc24e2988368ea",
+    "url": "/static/media/githubLight.bafee7d2.png"
+  },
+  {
+    "revision": "e1abd3fd61f58b45388bbaa4e95e3beb",
+    "url": "/static/media/patreon.e1abd3fd.png"
+  }
+]);
