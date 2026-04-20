dd110a632712870d538d37b13cb20775119fb78b
ethanzohar <ethanzohar9@gmail.com>
2026-04-19 23:21:07 -0400

    feat: add HTTP metrics and access log middleware

---
+++ backend/middleware/httpMetrics.js
@@ -0,0 +1,35 @@
+// backend/middleware/httpMetrics.js
+const { httpRequestsTotal, httpRequestDurationSeconds } = require('../helpers/metrics');
+const logger = require('../helpers/logger');
+
+function httpMetrics(req, res, next) {
+  const start = Date.now();
+
+  res.on('finish', () => {
+    const route = (req.route && req.route.path) ? req.route.path : req.path;
+    const durationMs = Date.now() - start;
+
+    httpRequestsTotal.inc({
+      method: req.method,
+      route,
+      status_code: res.statusCode,
+    });
+
+    httpRequestDurationSeconds.observe(
+      { method: req.method, route },
+      durationMs / 1000
+    );
+
+    logger.info({
+      event: 'http_request',
+      method: req.method,
+      route,
+      statusCode: res.statusCode,
+      durationMs,
+    }, 'HTTP request');
+  });
+
+  next();
+}
+
+module.exports = httpMetrics;
